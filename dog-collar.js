82ce8746c9cd3cf5984408c7daed46c257fbef92
Stephanie Nemeth <traumverloren@gmail.com>
2017-11-02 18:29:01 +0100

    change data pin

--- dog-collar.js
+++ dog-collar.js
@@ -1,4 +1,4 @@
-let strip = light.createNeoPixelStrip(pins.A0, 9, NeoPixelMode.RGBW);
+let strip = light.createNeoPixelStrip(pins.A1, 9, NeoPixelMode.RGBW);
 
 strip.setPixelColor(0, Colors.Red);
 strip.setPixelColor(1, Colors.Orange);
@@ -9,14 +9,13 @@ strip.setPixelColor(4, Colors.Blue);
 strip.setPixelColor(6, Colors.Indigo);
 strip.setPixelColor(7, Colors.Violet);
 strip.setPixelColor(8, Colors.Pink);
-
 strip.show();
 
 loops.forever(function() {
   light.pixels.showAnimation(light.animation(LightAnimation.Rainbow), 100);
   strip.move(LightMove.Rotate, 1);
-  loops.pause(100);
 });
+
 input.onGesture(Gesture.Shake, function() {
   light.pixels.stopAllAnimations();
   light.pixels.showAnimation(light.animation(LightAnimation.Sparkle), 1000);
