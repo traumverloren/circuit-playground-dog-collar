41626513317c248f1bcb8659b4ea8dc8d438dafd
Stephanie Nemeth <traumverloren@gmail.com>
2017-11-12 13:45:05 +0100

    update readme and js file

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # Circuit Playground Express + NeoPixels DiskoDogCollar ✨
 
+![dog wearing a rainbow led collar](wilburanimation.gif)
+
 A LED dog collar project to try out [MS MakeCode](https://makecode.adafruit.com/), Javascript, Circuit Playground Express and RGBW Neopixels (30/m strip)
 
+
+##### See the full build blogpost ---> [here](https://medium.com/@secretsquirrel/i-made-a-led-dog-collar-with-javascript-1c10ff973550).
+
+### Wiring Diagram:
+
+![fritzing diagram of collar and leds](circuitplaygroundcollar.png)
+
+### Coding the Collar:
+
+I coded the collar using the JavaScript editor and the [MakeCode API](https://makecode.adafruit.com/reference/) reference docs. I wanted the collar to do a few different things:
+
+1. It should continually run a rainbow 🌈 animation on the board and strip’s LEDs.
+2. If it detects a shake, it should sparkle ✨ randomly on the strip for 1 second.
+3. If I press button A, it will turn off all the LEDs.
+4. If I press button B, it will turn on the rainbow animations on all LEDs again.
+
+### Collar Demo:
+
+![demoing the collar's functions](collaranimation.gif)
+
 ---
+### One Happy Little Dog! 👇
+
+![Wilbur!](wilburparty.gif)
+
+---
+
 A project by [✨ Stephanie ✨](https://stephanie.lol)
--- dog-collar.js
+++ dog-collar.js
@@ -1,22 +1,47 @@
 let strip = light.createNeoPixelStrip(pins.A1, 9, NeoPixelMode.RGBW);
+let isPaused = false;
+let needsSetup = true;
 
-strip.setPixelColor(0, Colors.Red);
-strip.setPixelColor(1, Colors.Orange);
-strip.setPixelColor(2, Colors.Yellow);
-strip.setPixelColor(3, Colors.Green);
-strip.setPixelColor(4, Colors.Blue);
-strip.setPixelColor(4, Colors.Blue);
-strip.setPixelColor(6, Colors.Indigo);
-strip.setPixelColor(7, Colors.Violet);
-strip.setPixelColor(8, Colors.Pink);
-strip.show();
+const setupStrip = () => {
+  strip.setPixelColor(0, Colors.Red);
+  strip.setPixelColor(1, Colors.Orange);
+  strip.setPixelColor(2, Colors.Yellow);
+  strip.setPixelColor(3, Colors.Green);
+  strip.setPixelColor(4, Colors.Blue);
+  strip.setPixelColor(5, Colors.Blue);
+  strip.setPixelColor(6, Colors.Indigo);
+  strip.setPixelColor(7, Colors.Violet);
+  strip.setPixelColor(8, Colors.Pink);
+  strip.show();
+  needsSetup = false;
+};
 
-loops.forever(function() {
-  light.pixels.showAnimation(light.animation(LightAnimation.Rainbow), 100);
-  strip.move(LightMove.Rotate, 1);
+loops.forever(() => {
+  if (needsSetup) {
+    setupStrip();
+  }
+
+  if (!isPaused) {
+    light.showAnimation(light.rainbowAnimation, 100);
+    strip.move(LightMove.Rotate, 1);
+  }
 });
 
-input.onGesture(Gesture.Shake, function() {
-  light.pixels.stopAllAnimations();
-  light.pixels.showAnimation(light.animation(LightAnimation.Sparkle), 1000);
+input.onGesture(Gesture.Shake, () => {
+  strip.stopAllAnimations();
+  strip.showAnimation(light.sparkleAnimation, 1000);
+  needsSetup = true;
+});
+
+input.buttonA.onEvent(ButtonEvent.Click, () => {
+  strip.stopAllAnimations();
+  light.stopAllAnimations();
+  strip.clear();
+  light.clear();
+  isPaused = true;
+});
+
+input.buttonB.onEvent(ButtonEvent.Click, () => {
+  isPaused = false;
+  needsSetup = true;
 });
